69372445cf690b9773ea1b35bf975bec25368965
Luís Serrante <serrante@gmail.com>
2020-09-02 17:22:17 -0300

    Adjust in the cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import routes from './routes';
+
 import AppError from './errors/AppError';
 
 import createConnection from './database';
@@ -14,8 +16,8 @@ createConnection();
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors())
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
