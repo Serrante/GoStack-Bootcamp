33a9dcca14e83ec67d4a38489ec0ce43f02fdaf2
Luís Serrante <serrante@gmail.com>
2020-09-06 21:27:41 -0300

    Finishing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
 import GlobalStyles from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
-      <SignIn />
-    </AuthProvider>
-    <GlobalStyles />
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+
+      <GlobalStyles />
+    </Router>
   </>
 );
 
