124eadec87f0b3b19a09a2be43936fa75c794ec6
Luís Serrante <serrante@gmail.com>
2020-09-26 11:25:00 -0300

    List providers

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUsersDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../entities/Users';
 
@@ -26,8 +27,26 @@ class UsersRepository implements IUsersRepository {
     return user;
   }
 
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users = [];
+
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
+  }
+
   public async create(userData: ICreateUsersDTO): Promise<User> {
-    const user = await this.ormRepository.create(userData);
+    const user = this.ormRepository.create(userData);
 
     await this.ormRepository.save(user);
 
